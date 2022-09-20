cfd53757a9174f5950fbc5866df12d339daef7ee
Bruno de Souza <109471488+bsantuz@users.noreply.github.com>
2022-09-19 21:30:07 -0300

    add cors

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,11 @@
 import Axios from 'axios'
 
 export default async function API(url, callback){
+    Axios.defaults.headers.get['Content-Type'] ='application/json;charset=utf-8';
+    Axios.defaults.headers.get['Access-Control-Allow-Origin'] = '*';
     await Axios
-            .get(url)
+            .get(url,{
+                headers: {"Access-Control-Allow-Origin": "*"} 
+            })
             .then(response => callback(response.data))
 }
